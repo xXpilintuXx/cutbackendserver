3ccbdf614697e6b82ecd6417f5843da6a0278a1c
xXpilintuXx <ema-nuel06@hotmail.com>
2019-11-19 16:53:13 -0600

    Agregado modulo para resolver problema CORS

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const passport = require('passport');
+const session = require('express-session');
+const cors = require('cors');
+const MySqlStore = require('express-mysql-session');	
+const { database } = require('./config/keys');
+
 
 //init
 const app = express();
@@ -14,6 +20,12 @@ app.set('port', process.env.PORT || 3000);
 
 
 //middlewares
+app.use(session({
+	secret: 'xxSession',
+	resave: false,
+	saveUninitialized: false,
+	store: new MySqlStore(database)
+}))
 app.use(morgan('dev'));
 app.use(express.urlencoded({
 	extended: false
@@ -22,7 +34,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
 	extended: true
 }));
-
+app.use(cors());
+app.use(passport.initialize());
+app.use(passport.session());
 
 //rutas
 app.use(require('./routes/crud'));
