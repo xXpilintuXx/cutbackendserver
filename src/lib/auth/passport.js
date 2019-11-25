bf9f24001995f9b85a4648c804ea1ccc94cc94e3
xXpilintuXx <ema-nuel06@hotmail.com>
2019-11-25 11:16:38 -0600

    Modificado passport.js

---
+++ src/lib/auth/passport.js
@@ -0,0 +1,21 @@
+
+
+
+
+
+
+
+
+
+
+
+
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+  });
+  
+  passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+      done(err, user);
+    });
+  });
